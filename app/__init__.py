d218bcae1cdf6e14962eab5407ddf036de38bd2d
blackrose <wahomekelving@gmail.com>
2019-02-11 22:35:56 +0300

    Added create app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
-from flask_sqlalchemy import SqlAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
+def create_app(config_name):
+    app = Flask(__name__)
+
+
+# initializing flask extensions
+bootstrap.iniy_app(app)
+db.init_app(app)
 
 bootstrap = Bootstrap()
-db = SqlAlchemy()
+db = SQLAlchemy()
