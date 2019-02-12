16aed6ba8fab7bb7d285fd9debc851b9dfd82efe
blackrose <wahomekelving@gmail.com>
2019-02-12 23:43:21 +0300

    Rendered the signup form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,35 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
+from ..models import User
+from .forms import Signupform,LoginForm
+from .. import db
 from . import auth
+from flask_login import login_user,logout_user,login_required
+from ..email import mail_message
 
 @auth.route('/login')
 def login():
-    return render_template('auth/login.html')
+login_form=LoginForm()
+     if login_form.validate_on_submit():
+          user=User.query.filter_by(email=login_form.email.data).first()
+          if user is not None and user.verify_password(login_form.password.data):
+               login_user(user,login_form.remember.data)
+               return redirect(request.args.get('next') or url_for('main.index'))
+
+          flash('Invalid username or password')
+
+     title="New Account "
+
+     return render_template('auth/login.html' ,login_form=login_form,title=title)
+
+
+@auth.route('/signup',methods=['GET','POST'])
+def signup():
+     form=SignupForm()
+     if form.validate_on_submit():
+          user=User(email=form.email.data,username=form.username.data,password=form.password.data)
+          db.session.add(user)
+          db.session.commit()
+
+          return redirect(url_for('auth.login'))
+          title="New account"
+     return render_template('auth/signup.html',signup_form = form)
