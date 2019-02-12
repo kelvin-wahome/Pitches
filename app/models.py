f25e6d6a6e4592d1fcb80ba8466ee355f9533a5a
blackrose <wahomekelving@gmail.com>
2019-02-12 10:52:09 +0300

    Updated user model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email=db.Column(db.String(255),unique=True,index=True)
+	bio=db.Column(db.String(255))
+	profile_pic_path=db.Column(db.String(255))
+	pass_secure = db.Column(db.String(255))
+	pitchs=db.relationship('Pitch',backref='user',lazy='dynamic')
 
 
     def __repr__(self):
