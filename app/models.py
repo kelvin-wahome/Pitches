7e07eafddc3c6ca0517ff0eae42f9a7eca6c57d1
blackrose <wahomekelving@gmail.com>
2019-02-12 22:12:34 +0300

    Added Usermix

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
 
 from . import db
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
