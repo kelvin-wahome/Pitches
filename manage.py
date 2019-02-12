4b5408ab39812643fc4ee6c2bb65d358208d9312
blackrose <wahomekelving@gmail.com>
2019-02-12 11:20:12 +0300

    Initialized migrate

--- manage.py
+++ manage.py
@@ -1,11 +1,20 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
+from flask_migrate import Migrate,MigrateCommand
+
 
 
 # Creating app instance
 app = create_app('development')
+
+
 manager = Manager(app)
+manager.add_command('server',Server)
+
+#initializing the Migrate
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 @manager.shell
