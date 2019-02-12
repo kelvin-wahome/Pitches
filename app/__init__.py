8ba5596edf3ad5367c3abc235f1727896ea8c93f
blackrose <wahomekelving@gmail.com>
2019-02-12 09:57:58 +0300

    Added nav bar

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__)
 
 
-# initializing flask extensions
-bootstrap.iniy_app(app)
-db.init_app(app)
-
-bootstrap = Bootstrap()
-db = SQLAlchemy()
+    return app
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key)
+    id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
 
