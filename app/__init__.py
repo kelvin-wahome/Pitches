8b7898f8ca6465ff2623707719fb3ce274c0a340
blackrose <wahomekelving@gmail.com>
2019-02-12 17:10:53 +0300

    Registered auth blueprint instance

--- app/__init__.py
+++ app/__init__.py
@@ -19,13 +19,16 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
-    # Will add the views and forms
+
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    
+
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
 
 
     return app
