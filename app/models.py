00f2c681ea535d1c50c30c99117ca1ba3cfb2215
blackrose <wahomekelving@gmail.com>
2019-02-12 17:04:51 +0300

    Added an authentication blueprint

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
 
@@ -14,11 +14,11 @@ class User(db.Model):
     pitchs = db.relationship('Pitch', backref='user', lazy='dynamic')
 
     @property
-	def password(self):
-		raise AttributeError('You cannot read the attribute')
+    def password(self):
+            raise AttributeError('You cannot read the attribute')
 
-    def verify_password(self,password):
-		return check_password_hash(self.pass_secure,password)
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
@@ -26,17 +26,17 @@ class User(db.Model):
 
 class Category(db.Model):
     __tablename__ = 'categories'
-    id = db.Column(db.Integer,primary_key = True)
-    review=db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(255))
 
 
 class Pitch(db.Model):
-	__tablename__='pitches'
-	id=db.Column(db.Integer,primary_key=True)
-	head=db.Column(db.String(255))
-	body=db.Column(db.String(255))
-	user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
-	category_id=db.Column(db.Integer,db.ForeignKey('categories.id'))
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key=True)
+    head = db.Column(db.String(255))
+    body = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
-	def __repr__(self):
-		return f'User{self.review}'
+    def __repr__(self):
+        return f'User{self.review}'
