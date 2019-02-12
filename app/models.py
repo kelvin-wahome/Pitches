bad468d31bd231bce0c3fd8abcd39998cf204a92
blackrose <wahomekelving@gmail.com>
2019-02-12 17:00:46 +0300

    Added a method to verify passwords

--- app/models.py
+++ app/models.py
@@ -17,6 +17,9 @@ class User(db.Model):
 	def password(self):
 		raise AttributeError('You cannot read the attribute')
 
+    def verify_password(self,password):
+		return check_password_hash(self.pass_secure,password)
+
     def __repr__(self):
         return f'User {self.username}'
 
