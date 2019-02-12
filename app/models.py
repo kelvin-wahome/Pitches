8609831efaa76877dcc4743c4d10d8e2d4cda4b7
blackrose <wahomekelving@gmail.com>
2019-02-12 15:30:02 +0300

    Defined blue print on init in main

--- app/models.py
+++ app/models.py
@@ -3,14 +3,13 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
-    email=db.Column(db.String(255),unique=True,index=True)
-	bio=db.Column(db.String(255))
-	profile_pic_path=db.Column(db.String(255))
-	pass_secure = db.Column(db.String(255))
-	pitchs=db.relationship('Pitch',backref='user',lazy='dynamic')
-
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    pitchs = db.relationship('Pitch', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -19,6 +19,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User ,Category = Category,Pitch = Pitch)
 if __name__ == '__main__':
     manager.run()
