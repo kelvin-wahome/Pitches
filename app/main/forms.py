0e6d59db0045a161c825e9cd65f380dea05056e0
blackrose <wahomekelving@gmail.com>
2019-02-13 09:50:32 +0300

    Added update profile class with bio text field

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you', validators=[Required()])
+    submit = SubmitField('submit')
