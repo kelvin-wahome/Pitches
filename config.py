fc4530798f1f48cfa685fafd05f0dfda79aae86d
blackrose <wahomekelving@gmail.com>
2019-02-13 10:16:59 +0300

    Added UPLOADED_PHOTOS_DEST config to specify destination to store our images

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 import os
 
+
 class Config:
-        SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 
 class ProdConfig(Config):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+
 class DevConfig(Config):
     pass
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig
 }
