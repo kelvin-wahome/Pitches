40bc389ab50e62bb9ecdc8a6133ec3e8a4200bd9
blackrose <wahomekelving@gmail.com>
2019-02-13 10:33:15 +0300

    Added a route to to process submission requests

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from flask_login import login_required
-from .forms import pitchIdea,UpdateProfile
+from .forms import pitchIdea, UpdateProfile
+from .. import db, photos
+
 
 from ..models import User, Pitch, Category
 
@@ -20,19 +22,32 @@ def profile(uname):
 
     return render_template('profile/update.html', user=user.username, form=form)
 
-@main.route('/user/<uname>/update',methods=['GET','POST'])
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
 @login_required
 def update_profile(uname):
-    user=User.query.filter_by(username=uname).first()
+    user = User.query.filter_by(username=uname).first()
     if user is None:
         abort(404)
-    form=UpdateProfile()
+    form = UpdateProfile()
 
     if form.validate_on_submit():
-        user.bio=form.bio.data
+        user.bio = form.bio.data
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('.profile',uname=user.username,form=form))
+        return redirect(url_for('.profile', uname=user.username, form=form))
 
-    return render_template('profile/update.html',form=form,user=user)
+    return render_template('profile/update.html', form=form, user=user)
+
+
+@main.route('/user/<uname>/update/pic',methods=['POST'])
+@login_required
+def  update_pic(uname):
+    user=User.query.filter_by(username=uname).first()
+    if 'photos' in request.files:
+        filename=photos.save(request.files['photo'])
+        path=f'photos/{filename}'
+        user.profile_pic_path=path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
