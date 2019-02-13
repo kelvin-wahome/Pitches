36bc5fe47a97af30cef156e5c2877a284772413a
blackrose <wahomekelving@gmail.com>
2019-02-13 09:56:01 +0300

    Added a view function to handle update request

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from flask_login import login_required
+from .forms import pitchIdea,UpdateProfile
+
 from ..models import User, Pitch, Category
 
 
@@ -17,3 +19,20 @@ def profile(uname):
         db.session.commit()
 
     return render_template('profile/update.html', user=user.username, form=form)
+
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+    user=User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    form=UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio=form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username,form=form))
+
+    return render_template('profile/update.html',form=form,user=user)
