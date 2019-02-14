45e4be8a947d93cf9aa2051522c85ab7c7d2dde9
blackrose <wahomekelving@gmail.com>
2019-02-14 16:47:20 +0300

    updated configs

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
 
 class ProdConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_CYAN_URL")
 
 
 class DevConfig(Config):
